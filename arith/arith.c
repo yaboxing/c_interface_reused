fa06f29f70b5932df4d07022f5d9d3c776e7e172
zhexieren <zhe_xieren@163.com>
2018-12-17 11:47:11 +0800

    add(arith): arith

---
+++ arith/arith.c
@@ -0,0 +1,59 @@
+#include "arith.h"
+
+int Arith_max(int x, int y)
+{
+	return x>y? x : y;
+}
+
+int Arith_min(int x, int y)
+{
+	return x<y? x : y;
+}
+
+int Arith_div(int x, int y)
+{
+	int ret;
+
+	if(	(-13/5==-2) 	&& 
+		((x<0)!=(y<0))	&&
+		(x%y!=0)) {
+		ret = x/y - 1;
+	} else {
+		ret = x/y;
+	}
+
+	return ret;
+}
+
+int Arith_mod(int x, int y)
+{
+	int ret;
+
+	if(	(-13/5==-2) 	&& 
+		((x<0)!=(y<0))	&&
+		(x%y!=0)){
+		ret = x%y + y;
+	} else {
+		ret = x%y;
+	}
+
+	return ret;
+}
+
+int Arith_ceiling(int x, int y)
+{
+	int ret;
+
+	ret = Arith_div(x, y) + (x%y!=0);
+
+	return ret;
+}
+
+int Arith_floot(int x, int y)
+{
+	int ret;
+
+	ret = Arith_div(x, y);
+	
+	return ret;
+}
---
+++ arith/arith.h
@@ -0,0 +1,11 @@
+#ifndef __ARITHa_H__
+#define __ARITHa_H__
+
+extern int Arith_max(int x, int y);
+extern int Arith_min(int x, int y);
+extern int Arith_div(int x, int y);
+extern int Arith_mod(int x, int y);
+extern int Arith_ceiling(int x, int y);
+extern int Arith_floot(int x, int y);
+
+#endif
