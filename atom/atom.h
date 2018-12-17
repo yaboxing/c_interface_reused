a2db581a5401a7ee607f638dd0160a097bdbf80b
zhexieren <zhe_xieren@163.com>
2018-12-17 11:55:10 +0800

    add(atom): atom

---
+++ atom/atom.h
@@ -0,0 +1,9 @@
+#ifndef __ATOM_H__
+#define __ATOM_H__
+
+extern int Atom_length(const char* str);
+extern const char* Atom_new(const char* str, int len);
+extern const char* Atom_string(const char* str);
+extern const char* Atom_int(long n);
+
+#endif;
