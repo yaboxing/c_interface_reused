74e7f72a4375a16f6d8dcfd7959e559341bec4a9
zhexieren <zhe_xieren@163.com>
2018-12-17 11:55:22 +0800

    add(thread): thread

---
+++ thread/chan.h
@@ -0,0 +1,12 @@
+#ifndef __CHAN_H__
+#define __CHAN_H__
+
+#define T Chan_T
+typedef struct T *T;
+
+extern T Chan_new(void);
+extern int Chan_send(T c, const void* ptr, int size);
+extern int Chan_receive(T c, void* ptr, int size);
+
+#undef T
+#endif
---
+++ thread/sem.h
@@ -0,0 +1,19 @@
+#ifndef __SEM_H__
+#define __SEM_H__
+
+#define T Sem_T
+typedef struct T {
+	int count;
+	void* queue;
+} T;
+
+#define LOCK(mutex)	do{ Sem_T* _yymutex = &(mutex);Sem_wait(_yymutex);
+#define END_LOCK 	Sem_signal(_yymutex);} while(0)
+
+extern void Sem_init(T* s, int count);
+extern T *Sem_new(int count);
+extern void Sem_wait(T* s);
+extern void Sem_signal(T* s);
+
+#undef T;
+#endif
